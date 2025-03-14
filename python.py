ea25b1e6e6e7b0ffbf3528aad8ba66e19558597d
Suryaprakash1997 <157590817+Suryaprakash1997@users.noreply.github.com>
2025-03-14 14:19:05 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,10 @@
+import json
+
+def handler(event, context):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"  # ✅ Ensures HTTP response format
+        },
+        "body": json.dumps({"message": "This is manually deployed!- at 14.04.2025 14.20 PM"})
+        }
